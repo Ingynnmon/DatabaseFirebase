67b6a739d536bcd864c16ebc1d477f7585ca69ce
Ingynnmon <ingynnmon@ucsy.edu.mm>
2019-06-11 16:18:03 +0630

    saving data

---
+++ app/src/main/java/com/example/databasefirebase/Detail.kt
@@ -0,0 +1,3 @@
+package com.example.databasefirebase
+
+data class Detail(val id: String,val name: String, val rating: Int)
--- app/src/main/java/com/example/databasefirebase/MainActivity.kt
+++ app/src/main/java/com/example/databasefirebase/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.example.databasefirebase
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.*
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var name_et: EditText
+    lateinit var rating_bar: RatingBar
+    lateinit var save_btn: Button
+    lateinit var reference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        name_et = findViewById(R.id.name_et)
+        rating_bar = findViewById(R.id.rating_bar)
+        save_btn = findViewById(R.id.save_btn)
+
+        save_btn.setOnClickListener {
+            saveDetail()
+        }
+    }
+
+
+    private fun saveDetail() {
+        val hero_name = name_et.text.toString().trim()
+
+        if(hero_name.isEmpty()){
+            name_et.error="Please enter a name"
+            return
+        }
+
+        reference = FirebaseDatabase.getInstance().getReference("details")
+        val detailId =reference.push().key
+
+        val detail = Detail(detailId!!, hero_name,rating_bar.numStars)
+        reference.child(detailId).setValue(detail).addOnCompleteListener {
+            Toast.makeText(this, "Saved successfully", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
